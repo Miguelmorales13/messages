a2215708becfb829906cb201a14def998618877d
Miguelmorales13 <cacahuatisimo13@gmail.com>
2022-10-02 15:43:51 -0500

    add all templates

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { TemplateEnum } from "./enums/template.enum";
 import { EmailService } from "./services/email/email.service";
 import { IEmailVerification } from "./models/email/IEmailVerification";
+import { IEmailSubscription } from "./models/email/IEmailSubscription";
+import { IEmailRecoveryPassword } from "./models/email/IEmailRecoveryPassword";
 
 @Injectable()
 export class AppService {
@@ -10,7 +12,18 @@ export class AppService {
   }
 
   async sendEmail(email: string, template: TemplateEnum, data: Object): Promise<string> {
-    await this.service.sendVerificationEmail(email, data as IEmailVerification);
+    switch (template) {
+      case TemplateEnum.recoveryPassword:
+        await this.service.sendRecoveryPassword(email, data as IEmailRecoveryPassword);
+        break;
+      case TemplateEnum.subscription:
+        await this.service.sendSubscription(email, data as IEmailSubscription);
+        break;
+      case TemplateEnum.verification:
+      default:
+        await this.service.sendVerificationEmail(email, data as IEmailVerification);
+        break;
+    }
     return "Hello World!";
   }
 }
